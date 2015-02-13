c732fbeda4909166fb4add74885a7146f94bfa15
Andre Nils <nilzao@gmail.com>
2015-02-13 10:42:38 -0300

    Rock Sample adaptado para composer

---
+++ examples/sample.php
@@ -0,0 +1,7 @@
+<?php
+// error_reporting(E_ALL | E_STRICT);
+// ini_set('display_errors', true);
+require_once dirname(dirname(dirname(__FILE__))).'/core/src/Rock/Core/AutoLoad.php';
+
+new Rock_Sample_Front();
+// new Rock_Sample_Front('Index', 'handle', 'w3c');
---
+++ examples/ws/Index.php
@@ -0,0 +1,3 @@
+<?php
+require_once dirname(dirname(dirname(dirname(__FILE__)))) . '/phpwsdl/src/Rock/PhpWsdl/class.phpwsdl.php';
+PhpWsdl::RunQuickMode(dirname(dirname(dirname(dirname(__FILE__)))) . '/sample/src/Rock/Sample/Ctr/Index.php');
